c4441dd599c5d28a1fd8cafb7a4a374048572947
j0811s <whitedog0811@hotmail.co.jp>
2020-04-17 01:08:28 +0900

    関数分岐処理

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 require('intersection-observer');
 import objectFitImages from 'object-fit-images';
+objectFitImages();
 import hamburgerMenu from './hamburgerMenu';
 import backgroundMouseMove from './backgroundMouseMove';
 import fullScreenScroll from './fullScreenScroll';
@@ -7,93 +8,85 @@ import headerTextColor from './headerTextColor';
 import progressBar from './progressBar';
 import Barba from "barba.js";
 
-
-//ロード後の黒幕
-const blackCurtain = () => {
-  const targetElement = document.getElementById('js_top') || document.getElementById('js_about');
-  targetElement.classList.add('is_loaded');
-  if (targetElement.classList.contains('is_loaded')) document.body.classList.remove('is_lock');
-};
-//ヒーローイメージ拡大 => about.html
-const keyVisualScale = () => {
-  const keyVisual = document.getElementById('js_keyVisualSize');
-  if (keyVisual) keyVisual.classList.add('is_sizeUp');
-};
-// 実行
-objectFitImages();
-window.addEventListener('DOMContentLoaded', hamburgerMenu);
-window.addEventListener('load', blackCurtain);
-
-
-
-// barba.js 参考：https://qiita.com/kokushin/items/a9cca2ef52e6e927115d
-Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container, newPageRawHTML) {
-  if (Barba.HistoryManager.history.length === 1) {
-    return;
+((d, w) => {
+  // ページごとの処理を分別
+  const pageEvents = {
+    allPage: () => {
+      const load = d.querySelector('.js_curtain');
+      load.classList.add('is_loaded');
+      if (load.classList.contains('is_loaded')) d.body.classList.remove('is_lock');
+    },
+    topPage: () => {
+      d.body.style.overflow = '';
+      fullScreenScroll(); //スクリーンスクロール
+      backgroundMouseMove(); //背景画像座標
+      if (d.getElementById('js_header').classList.contains('is_instagram') || d.getElementById('js_header').classList.contains('is_color')) {
+        d.getElementById('js_header').classList.remove('is_instagram');
+        d.getElementById('js_header').classList.remove('is_color');
+        w.removeEventListener('scroll', headerTextColor);
+        w.removeEventListener('scroll', progressBar);
+      }
+    },
+    aboutPage: () => {
+      d.body.style.height = '';
+      d.body.style.overflow = 'scroll';
+      const keyVisual = d.getElementById('js_keyVisualSize');
+      if (keyVisual) keyVisual.classList.add('is_sizeUp');
+      d.getElementById('js_header').classList.add('is_instagram');
+      w.addEventListener('scroll', headerTextColor); //ヘッダーナビの文字色変更
+      w.addEventListener('scroll', progressBar); //プログレスバーのゲージ
+    }
   }
-  // headタグ差し替え
-  const head = document.head,
-    newPageRawHead = newPageRawHTML.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0],
-    newPageHead = document.createElement('head');
-  newPageHead.innerHTML = newPageRawHead;
-  const headTags = [
-    "meta[name='description']",
-    "meta[property^='og']"
-  ].join(',');
-  const oldHeadTags = head.querySelectorAll(headTags);
-  for (let i = 0; i < oldHeadTags.length; i++) {
-    head.removeChild(oldHeadTags[i]);
-  }
-  let newHeadTags = newPageHead.querySelectorAll(headTags);
-  for (let i = 0; i < newHeadTags.length; i++) {
-    head.appendChild(newHeadTags[i]);
-  }
-});
 
-//index.htmlに遷移する時
-const top = Barba.BaseView.extend({
-  namespace: 'top',
-  onEnter: function () {
-    window.addEventListener('load', fullScreenScroll, false);
-    window.addEventListener('load', backgroundMouseMove, false);
-  },
-  onEnterCompleted: () => {
-    blackCurtain();
-    backgroundMouseMove();
-    fullScreenScroll();
-  },
-  onLeave: function () {
-  },
-  onLeaveCompleted: function() {
-    window.removeEventListener('load', fullScreenScroll, false);
-    window.removeEventListener('load', backgroundMouseMove, false);
+  // idで存在判定 > pageEvents()
+  const checkEvents = () => {
+    if (d.getElementById('js_top')) {
+      pageEvents.topPage();
+    } else if (d.getElementById('js_about')) {
+      pageEvents.aboutPage();
+    }
+    pageEvents.allPage();
   }
-});
-top.init();
 
-//about.htmlに遷移する時
-const about = Barba.BaseView.extend({
-  namespace: 'about',
-  onEnter: function() {
-    window.addEventListener('load', keyVisualScale, false);
-    window.addEventListener('scroll', headerTextColor, false);
-    window.addEventListener('scroll', progressBar, false);
-  },
-  onEnterCompleted: () => {
-    document.body.style.height = '';
-    blackCurtain();
-    keyVisualScale();
-    headerTextColor();
-    progressBar();
-  },
-  onLeave: function() {
-  },
-  onLeaveCompleted: function() {
-    window.removeEventListener('load', keyVisualScale, false);
-    window.removeEventListener('scroll', headerTextColor, false);
-    window.removeEventListener('scroll', progressBar, false);
+  //初回実行用
+  const init = () => {
+    hamburgerMenu();
+    checkEvents();
   }
-});
-about.init();
+  w.addEventListener('load', init);
 
-Barba.Pjax.start();
+
+
+  // barba.js
+  // 参考：https://qiita.com/kokushin/items/a9cca2ef52e6e927115d
+  Barba.Pjax.start();
+
+  // 遷移処理が完了し、以前の要素が削除されたとき
+  Barba.Dispatcher.on('transitionCompleted', function(){
+    checkEvents();
+  });
+
+  // 新しい要素が読み込まれ、コンテナ要素に挿入されたとき
+  Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container, newPageRawHTML) {
+    if (Barba.HistoryManager.history.length === 1) {
+      return;
+    }
+    // headタグ差し替え
+    const head = d.head,
+      newPageRawHead = newPageRawHTML.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0],
+      newPageHead = d.createElement('head');
+    newPageHead.innerHTML = newPageRawHead;
+    const headTags = [
+      "meta[name='description']",
+      "meta[property^='og']"
+    ].join(',');
+    const oldHeadTags = head.querySelectorAll(headTags);
+    for (let i = 0; i < oldHeadTags.length; i++) {
+      head.removeChild(oldHeadTags[i]);
+    }
+    let newHeadTags = newPageHead.querySelectorAll(headTags);
+    for (let i = 0; i < newHeadTags.length; i++) {
+      head.appendChild(newHeadTags[i]);
+    }
+  });
+})(document, window);
