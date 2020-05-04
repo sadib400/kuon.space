3301ab1e86a570582f33cc8b0e1774f331a5c2de
j0811s <whitedog0811@hotmail.co.jp>
2020-05-04 16:14:14 +0900

    見た目戻し対応 一部

--- src/js/backgroundMouseMove.js
+++ src/js/backgroundMouseMove.js
@@ -6,31 +6,35 @@ export default function () {
 
     /** マウスムーブで背景画像を移動 */
     d.body.addEventListener('mousemove', (ele) => {
-      let bgX = ele.pageX * .02;
-      let bgY = ele.pageY * .04;
-      let textX = ele.pageX * .01;
-      let textY = ele.pageY * .01;
-      let moonX = ele.pageX * .02;
-      let moonY = ele.pageY * .04;
-      let cloudX = ele.pageX * .03;
-      let cloudY = ele.pageY * .03;
-      let deepCloudX = ele.pageX * .03;
-      let deepCloudY = ele.pageY * .05;
+      let bgX = ele.pageX * .1;
+      let bgY = ele.pageY * .1;
 
       querySliceAll('.js_backgroundPosition').forEach((val) => {
-        val.style.transform = 'translate' + '(' + '-' + bgX + 'px' + ',' + '-' + bgY + 'px' + ')';
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .3 + 'px' + ',' + '-' + bgY * .3 + 'px' + ',' + '0' + ')';
+      });
+      querySliceAll('.js_backgroundPosition.js_star').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .1 + 'px' + ',' + '-' + bgY * .1 + 'px' + ',' + '0' + ')';
       });
       querySliceAll('.js_background_text').forEach((val) => {
-        val.style.transform = 'translate' + '(' + '-' + textX + 'px' + ',' + '-' + textY + 'px' + ')';
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .2 + 'px' + ',' + '-' + bgY * .2 + 'px' + ',' + '0' + ')';
       });
       querySliceAll('.js_moon').forEach((val) => {
-        val.style.transform = 'translate' + '(' + '-' + moonX + 'px' + ',' + '-' + moonY + 'px' + ')';
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .5 + 'px' + ',' + '-' + bgY * .5 + 'px' + ',' + '0' + ')';
       });
-      querySliceAll('.js_cloud').forEach((val) => {
-        val.style.transform = 'translate' + '(' + '-' + cloudX + 'px' + ',' + '-' + cloudY + 'px' + ')';
+      querySliceAll('.js_cloud.js_upperLeft').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .4 + 'px' + ',' + '-' + bgY * .4 + 'px' + ',' + '0' + ')';
       });
-      querySliceAll('.js_deep_cloud').forEach((val) => {
-        val.style.transform = 'translate' + '(' + '-' + deepCloudX + 'px' + ',' + '-' + deepCloudY + 'px' + ')';
+      querySliceAll('.js_cloud.js_upperRight').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .3 + 'px' + ',' + '-' + bgY * .3 + 'px' + ',' + '0' + ')';
+      });
+      querySliceAll('.js_deep_cloud.js_lowerLeft').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .7 + 'px' + ',' + '-' + bgY * .7 + 'px' + ',' + '0' + ')';
+      });
+      querySliceAll('.js_deep_cloud.js_lowerRight').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .6 + 'px' + ',' + '-' + bgY * .6 + 'px' + ',' + '0' + ')';
+      });
+      querySliceAll('.js_deep_cloud.js_lowerCenter').forEach((val) => {
+        val.style.transform = 'translate3d' + '(' + '-' + bgX * .8 + 'px' + ',' + '-' + bgY * .8 + 'px' + ',' + '0' + ')';
       });
     });
   })(document, window);
