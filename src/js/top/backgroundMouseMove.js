86588618ef748815f8713decf9a82fec363089df
j0811s <whitedog0811@hotmail.co.jp>
2020-06-21 17:10:37 +0900

    background mouseMove

--- src/js/top/backgroundMouseMove.js
+++ src/js/top/backgroundMouseMove.js
@@ -1,19 +1,29 @@
 import { d } from '../common/util';
-export default function() {
+import anime from 'animejs';
+export default function () {
   /** マウス移動で背景画像を逆追従
    * @param {Object} id 対象のセレクタ
    * @param {Number} movement 移動量の数値
-   */
+  */
   const backgroundMouseMove = (id, movement = .01) => {
     d.body.addEventListener('mousemove', (event) => {
       const mouseX = event.pageX * movement;
       const mouseY = event.pageY * movement;
       const bgX = Math.floor((mouseX * 10)) / 10;
       const bgY = Math.floor((mouseY * 10)) / 10;
-      if (d.getElementById(id)) d.getElementById(id).style.transform = 'translate3d' + '(' + '-' + bgX + 'px' + ',' + '-' + bgY + 'px' + ',' + 0 + ')';
+      console.log(bgX, bgY);
+      if (d.getElementById(id)) {
+        anime({
+          targets: d.getElementById(id),
+          translateX: '-' + mouseX,
+          translateY: '-' + mouseY,
+          translateZ: 0,
+          easing: 'easeOutCirc'
+        });
+      }
     });
   }
-
+  
   backgroundMouseMove('js_background', .03);
   backgroundMouseMove('js_background_star');
   backgroundMouseMove('js_background_text', .02);
